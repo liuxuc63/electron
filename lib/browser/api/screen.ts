de688f73ee09685085c72a02c44ba9fd67524284
Shelley Vohr <shelley.vohr@gmail.com>
2020-12-03 15:15:05 -0800

    Review feedback

--- lib/browser/api/screen.ts
+++ lib/browser/api/screen.ts
@@ -5,8 +5,9 @@ const { createScreen } = process._linkedBinding('electron_common_screen');
 let _screen: Electron.Screen;
 
 const ensureScreen = () => {
-  _screen = createScreen();
-  EventEmitter.call(_screen);
+  if (_screen === undefined) {
+    _screen = createScreen();
+  }
 };
 
 // We can't call createScreen until after app.on('ready'), but this module
@@ -15,7 +16,7 @@ const ensureScreen = () => {
 // we export a proxy which lazily calls createScreen on first access.
 export default new Proxy({}, {
   get: (target, prop: keyof Electron.Screen) => {
-    if (_screen === undefined) ensureScreen();
+    ensureScreen();
     const v = _screen[prop];
     if (typeof v === 'function') {
       return v.bind(_screen);
@@ -23,19 +24,18 @@ export default new Proxy({}, {
     return v;
   },
   ownKeys: () => {
-    if (_screen === undefined) ensureScreen();
+    ensureScreen();
     return Reflect.ownKeys(_screen);
   },
   getPrototypeOf: (target) => {
-    const targetPrototype = Reflect.getPrototypeOf(target);
-    return Object.assign(EventEmitter.prototype, targetPrototype);
+    return Object.setPrototypeOf(target, EventEmitter.prototype);
   },
-  has: (target, prop: string) => {
-    if (_screen === undefined) ensureScreen();
+  has: (_target, prop: string) => {
+    ensureScreen();
     return prop in _screen;
   },
   getOwnPropertyDescriptor: (target, prop: string) => {
-    if (_screen === undefined) ensureScreen();
+    ensureScreen();
     return Reflect.getOwnPropertyDescriptor(_screen, prop);
   }
 });
